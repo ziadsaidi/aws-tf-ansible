91ebddb830ce06824c3b0b906603ac740c264b11
saidi zied <zied.saidi@inov-tech.fr>
2025-04-16 18:33:51 +0200

    test

---
+++ infra/variables.tf
@@ -0,0 +1,17 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
+
+
+variable "public_key" {
+    type = string
+  
+}
