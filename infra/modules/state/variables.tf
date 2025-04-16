6121a3666df64caaaac0762799cec398bf0dd08b
saidi zied <zied.saidi@inov-tech.fr>
2025-04-16 22:31:52 +0200

    test pipeline

--- infra/modules/state/variables.tf
+++ infra/modules/state/variables.tf
@@ -2,12 +2,12 @@
 variable "state_bucket_name" {
   description = "Name of the S3 bucket for Terraform state"
   type        = string
-  default     = "todo-app-terraform-state"
+  default     = "todo-app-terraform-state-1223378"
 }
 
 variable "dynamodb_table_name" {
   description = "Name of the DynamoDB table for Terraform state locking"
   type        = string
-  default     = "todo-app-terraform-locks"
+  default     = "todo-app-terraform-12223333-locks"
 }
 
